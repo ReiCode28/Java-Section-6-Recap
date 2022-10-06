5969d9705d46287bd61fd2b1e699310db120ba47
randy <randyr_1993@yahoo.com>
2022-10-06 01:52:30 -0400

    ExamsTwo Complete

--- ExamsTwo.java
+++ ExamsTwo.java
@@ -3,7 +3,11 @@ public class ExamsTwo {
         
         System.out.println("It's time to take your 5th years exams. Please, take your seats.\n");
         //Task 1: Create an array that stores 5 students: "Malfoy", "Crabbe", "Goyle", "Pansy", "Dean"
-        
+        String[] students = {"Malfoy", "Crabbe", "Goyle", "Pansy", "Dean"};
+
+        for (int i = 0; i < students.length; i++){
+            System.out.println(students[i] + ", you will take seat " + i);
+        }
         /**Task 2: Using a for loop, assign each student a seat according to their index in the array.
          
           for  ( ...) { 
