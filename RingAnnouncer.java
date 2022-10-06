81d8b2381d77e50cf9625ab4154c404427455c5f
randy <randyr_1993@yahoo.com>
2022-10-06 03:54:04 -0400

    Completed RingAnnouncer

--- RingAnnouncer.java
+++ RingAnnouncer.java
@@ -1,7 +1,20 @@
 public class RingAnnouncer {
     public static void main(String[] args) {
         String[] record = {"WIN", "WIN", "WIN", "LOSS", "WIN", "WIN", "LOSS"};
+        
+        int wins = 0;
+        int losses = 0;
+        
+        for (int i = 0; i < record.length; i++){
+            if (record[i].equals("WIN")){
+                wins++;
+            } else if(record[i].equals("LOSS")){
+                losses++;
+            }
+        }
 
+        System.out.println("\nWith a professional record of " + wins + " Wins and " + losses + " losses.");
+        System.out.println("He is the pride of oracle: Java Fury!\n");
         /** Task 1 
          * 
          *   Using a for loop:
