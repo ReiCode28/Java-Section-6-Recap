ecae7fa92fce952e7171dde1839fb45b0dddc5a0
randy <randyr_1993@yahoo.com>
2022-10-06 03:40:56 -0400

    added functionality

--- HighScoreTwo.java
+++ HighScoreTwo.java
@@ -3,23 +3,21 @@ public class HighScoreTwo {
         int[] scores = {randomNumber(), randomNumber(), randomNumber(), randomNumber(), randomNumber(),
             randomNumber(), randomNumber(), randomNumber(), randomNumber(), randomNumber()};
         
-        System.out.print("Here are the scores: ");
+        System.out.println("\nHere are the scores: ");
         for (int i = 0; i < scores.length; i++) {
-            System.out.print(scores[i] + " ");
+            System.out.println(i+1 + ". " + scores[i]);
         }
     
         int highScore = 0;
+        int seat = 0;
         for (int i = 0; i < scores.length; i++) {
             if (scores[i] > highScore) {
                 highScore = scores[i];
+                seat = i + 1;
             }
         }
-        /** Task 1 – Find the person with the highest score
-         *  
-         *   • After obtaining the highest score, print: The highest score is:  <highScore>. Impressive!
-         *   • It's the gentleman in seat: <index>. Give that man a cookie!
-         *  
-         */
+        System.out.println("\n\nThe highest score is: " + highScore + ". Impressive!");
+        System.out.println("It's the gentleman in seat: " + seat + ". Give that man a cookie!");
     }
     
     public static int randomNumber() {
