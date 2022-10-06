625054b40350459540babf79166c480ca929a6db
randy <randyr_1993@yahoo.com>
2022-10-06 00:41:07 -0400

    Simple array practice complete

--- Exams.java
+++ Exams.java
@@ -2,10 +2,24 @@ public class Exams {
     public static void main(String[] args) {
         
         System.out.println("It's time to take your 5th year exams. Please, take your seats.\n");
-        //Task 1: Create an array that stores 5 students: "Harry", "Neville", "Ron", "Hermione", "Seamus"
 
-        //Task 2: Assign each student a seat according to their index in the array.
-        //           print: " <student at index 0> ", you will take seat 0. 
+        String[] students = {"Harry", "Neville", "Ron", "Hermione", "Seamus"};
+
+        System.out.println(students[0] + ", you will take seat 0.");
+        System.out.println(students[1] + ", you will take seat 1.");
+        System.out.println(students[2] + ", you will take seat 2.");
+        System.out.println(students[3] + ", you will take seat 3.");
+        System.out.println(students[4] + ", you will take seat 4."); 
 
     }
 }
+
+//just some simple array practice
+
+/** Expected Output: 
+Harry, you will take seat 0
+Neville, you will take seat 1
+Ron, you will take seat 2
+Hermione, you will take seat 3
+Seamus, you will take seat 4
+ */
