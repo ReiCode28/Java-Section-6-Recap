7a86fd5758dcc9104d1dbdef34decef61af70e86
randy <randyr_1993@yahoo.com>
2022-10-06 02:31:44 -0400

    Added interactivity

--- GrocerTwo.java
+++ GrocerTwo.java
@@ -1,20 +1,25 @@
+import java.util.Scanner;
+
 public class GrocerTwo {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
 
         String[] store = {"apples", "bananas", "candy", "chocolate", "coffee", "tea"};
         System.out.println("\nWelcome to Java Grocers. ");
         System.out.println("What can I help you find?\n");
-        //  Task 1: Set up Scanner, and pick up the user's response.  
 
+        String response = scan.nextLine();
+        
+        scan.close();
         
         for (int i = 0; i < store.length; i++) {
-          /* Task 2  
-                if element equals user's response  {
-                    println: \nWe have that in aisle:  <index>
-                    break;
-                }
-          */
+            if(store[i].equals(response)){
+                System.out.println("\nWe have " + store[i] + " in aisle: " + i);
+                break;
+            } else {
+                System.out.println("Sorry, we do not have " + response);
+                break;
+            }
         }
-
     }
 }
