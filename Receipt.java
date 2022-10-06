9a63e6b6204d8329e266eb2e60b9c441a950d4bc
randy <randyr_1993@yahoo.com>
2022-10-06 04:16:34 -0400

    Completed Receipt

--- Receipt.java
+++ Receipt.java
@@ -3,7 +3,6 @@ import java.util.Arrays;
 public class Receipt {
     public static void main(String[] args) {
 
-        //Task 1: Create two arrays (see Workbook article)
         String[] appleType = {"Gala", "Granny Smith", "Macintosh"};
         double[] applePrice = {1.99, 1.49, 1.29};
 
@@ -12,12 +11,5 @@ public class Receipt {
         for(int i = 0; i < appleType.length; i++){
             System.out.println(appleType[i] + ": $" + applePrice[i]);
         }
-        /* Task 2
-            Use a for loop to print each apple and the corresponding price.
-            
-            for {
-                println(\t<apple i >: $<price i >)
-            }
-        */
     }
 }
