75a4f00f049b55275a271045f3901fd744e232ee
randy <randyr_1993@yahoo.com>
2022-10-08 02:02:54 -0400

    Completed Weather

--- Weather.java
+++ Weather.java
@@ -4,14 +4,16 @@ public class Weather {
     public static void main(String[] args) {
         
         double[] celsius = {12.5, 14.5, 17.0, 21.0, 23.0, 18.5, 20.0};
-        double[] fahrenheit;
-        //Task 2, call celsiusToFahrenheit and store the result in the fahrenheit array. 
-
-        //Task 4, Call printTemperatures for celsius and fahrenheit.
+        double[] fahrenheit = celsiusToFahrenheit(celsius);
+        
+        System.out.println();
+        printTemperatures(celsius, "Celsius");
+        printTemperatures(fahrenheit, "Fahrenheit");
+        System.out.println();
     }
 
 
-    /** Task 1
+    /**
      * Function name: celciusToFahrenheit. 
      * @param celsius ( double[] )
      * @return fahrenheit ( double[] )
@@ -21,9 +23,17 @@ public class Weather {
      *    2. Copy all the values from celsius into the fahrenheit array.
      *    3. Update all the values in the array according to: F = (C/5 * 9) + 32.
      *    4. return the fahrenheit array.
-     */   
+     */  
+    public static double[] celsiusToFahrenheit(double[] celsius){
+        double[] fahrenheit = new double[celsius.length];
 
-    /** Task 3
+        for(int i = 0; i < celsius.length; i++){
+            fahrenheit[i] = (celsius[i] / 5 * 9) + 32;
+        }
+        return fahrenheit;
+    } 
+
+    /** 
      * Function name - printTemperatures
      * @param temp ( double[] )
      * @param type ( String ) can be either Celsius or Fahrenheit
@@ -34,6 +44,12 @@ public class Weather {
      *   2. Adds a new line of space after printing the contents of the array.
      *   3. (Task 5) Rounds every temperature value to two decimal places. 
      */
-
-
+    public static void printTemperatures(double[] temp, String type){
+        System.out.print(type + ": ");
+        for(int i = 0; i < temp.length; i++){
+            System.out.print(temp[i] + " ");
+        }
+        System.out.print("\n");
+        
+    }
 }
