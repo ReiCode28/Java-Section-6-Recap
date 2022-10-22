b83a40e79a6b106f800da1719c78192062e7bc9f
randy <randyr_1993@yahoo.com>
2022-10-22 13:23:02 -0400

    1000 random numbers stored in a 2D Array

--- RandomNumbers.java
+++ RandomNumbers.java
@@ -1,37 +1,47 @@
 public class RandomNumbers {
     public static void main(String[] args) {
-        // Task 3 – Create a 2D array with 100 rows and 10 columns.
+        // 2D array with 100 rows and 10 columns.
+        int[][] randomArr = new int[100][10];
 
 
-        /** Task 4
-         * 
-         * 1. Using a nested loop, populate the array with random numbers. 
-         * 2. Then, pass the updated array into print2DArray.  
-         * 
+        /**
+         *  nested loop populated with random numbers then 
+         * updating array into print2Darray
          */
 
-
-        
+        for(int i = 0; i < randomArr.length; i++){
+            for(int j = 0; j < randomArr[i].length; j++){
+                randomArr[i][j] = randomNumber();
+            }
+            print2DArray(randomArr);
+        } 
     }
 
-    /** Task 1
-     * Function name: randomNumber.
-     * 
-     * @return randomNumber (int)
-     * 
+    /** 
      * Inside the function:
-     *    1. return a random number between 1 and 100. 
+     *    1. returning a random number between 1 and 100. 
      */
 
+    public static int randomNumber(){
+        double randomNumber = Math.random()*100;
+        return (int)randomNumber;
+    } 
 
-    /** Task 2
-     * Function name: print2DArray
-     * @param array ( int[][] )
-     *
+
+    /** 
      * Inside the function:
      *
      *   1. prints the array such that:
      *        • Each row takes up one line.
      *        • There's a space between each value.
      */
+
+    public static void print2DArray(int[][] array) {
+        for(int i = 0; i < array.length; i++){
+            for(int j = 0; j < array[i].length; j++){
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.print("\n");
+        }
+    } 
 }
