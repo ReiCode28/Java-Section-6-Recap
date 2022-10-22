d89dc6cf71eb06e0c14d66474eed7938c395a87a
randy <randyr_1993@yahoo.com>
2022-10-22 12:12:36 -0400

    Prices.java with looped 2D array

--- Prices.java
+++ Prices.java
@@ -8,9 +8,22 @@ public class Prices {
             {8.99, 7.99, 9.49, 9.99, 10.99}
         };
 
-        System.out.println("Baking: " + Arrays.toString(arr[0]));
-        System.out.println("Beverage: " + Arrays.toString(arr[1]));
-        System.out.println("Cereals: " + Arrays.toString(arr[2]));
+        for (int i = 0; i < arr.length; i++){
+            switch (i) {
+                case 0: System.out.print("\tBaking: "); break;
+                case 1: System.out.print("\tBeverage: "); break;
+                case 2: System.out.print("\tCereals: "); break;
+            }
+            for(int j = 0; j < arr[i].length; j++){
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.print("\n");
+        }
 
+        // System.out.println("Baking: " + Arrays.toString(arr[0]));
+        // System.out.println("Beverage: " + Arrays.toString(arr[1]));
+        // System.out.println("Cereals: " + Arrays.toString(arr[2]));
+
+        
     }
 }
